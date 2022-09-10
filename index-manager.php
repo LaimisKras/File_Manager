46bc4c9a1f1cce59fe1f7242aa42445de9d03241
Laimis Krasauskas <laimis.kras@gmail.com>
2022-09-10 20:59:39 +0300

    Creating a new folder and upload file logic

--- index-manager.php
+++ index-manager.php
@@ -13,6 +13,68 @@ print('<br>');
 print('<h1 style="color:black; text-align:center;">File Manager</h1>');
 
 
+// creating a new folder
+$path=isset($_GET["path"]) ? './' . $_GET["path"] : './';
+    if (isset($_POST['createfolder'])) {
+        $foldername = ($_POST['createfolder']);
+    if (!file_exists($path . $foldername)) {
+        mkdir($path . "/" . $foldername);
+        print('<p class="warning">Folder was created!</p>');
+        header('refresh: 2');
+    } else if ($foldername) {
+        print('<p class="warning">Such folder already exists!</p>');
+        header('refresh: 2');
+    } else {
+        print('<p class="warning">Failed to create folder! Please write folder name!</p>');
+        header('refresh: 2');
+    }
+}
+print(' 
+    <form action="" method="POST" enctype="multipart/form-data">
+        <div class="createField">
+            <input type="text" name="createfolder" class="createInput" />
+            <input type="submit" name="submit" value="Create folder" class="createButton" />
+        </div>
+    </form>
+');
+
+
+// upload file logic
+if (isset($_FILES['image'])) {
+    $errors = "";
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+    $file_type = $_FILES['image']['type'];
+    $exploded = explode('.', $_FILES['image']['name']);
+    $file_ext = strtolower(end($exploded));
+    $extensions = ["jpeg", "jpg", "png"];
+    if (in_array($file_ext, $extensions) === false) {
+        $errors = '<p class="warningUpl">File format is not allowed, please choose a JPEG or PNG file!</p>';
+        header('refresh:2');
+    }
+    if ($file_size > 2097152) {
+        $errors = '<p class="warningUpl">File size must be smaller than 2 MB!</p>';
+        header('refresh: 2');
+    }
+    if (empty($errors) == true) {
+        move_uploaded_file($file_tmp, $path . $file_name);
+        echo '<p class="warningUpl">Success!Image uploaded</p>';
+        header('refresh: 2');
+    } else {
+        print_r($errors);
+    }
+}
+print('
+    <form action="" method="POST" enctype="multipart/form-data">
+        <div class="uploadField">
+            <input type="file" name="image" class="uploadInput" />
+            <input type="submit" value="Upload image" class="createButton" />
+        </div>
+    </form>
+');
+
+
 // folder and files list
 $path = isset($_GET["path"]) ? './' . $_GET["path"] : './';
 $files_and_dirs = scandir($path);
@@ -62,6 +124,7 @@ foreach ($files_and_dirs as $fnd) {
 }
 print('</table>');
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
